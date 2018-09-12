a0fbe343be7a56fb8a694831978b2a9b7b35248c
“dylanburkey” <dylanburkey@outlook.com>
2018-09-11 21:28:14 -0400

    add files

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,32 @@
-CREATE SCHEMA `burgers_db` ;
+/*
+ Navicat MySQL Data Transfer
 
-CREATE TABLE `burgers_db`.`burgers` (
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50721
+ Source Host           : localhost:3306
+ Source Schema         : burgers_db
+
+ Target Server Type    : MySQL
+ Target Server Version : 50721
+ File Encoding         : 65001
+
+ Date: 10/18/2018 21:13:16
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for burgers
+-- ----------------------------
+DROP TABLE IF EXISTS `burgers`;
+CREATE TABLE `burgers` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `burger_name` VARCHAR(255) NOT NULL,
-  `devoured` TINYINT(1) NULL,
-  `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`));
+  `burger_name` varchar(100) NOT NULL,
+  `devoured` tinyint(1) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
